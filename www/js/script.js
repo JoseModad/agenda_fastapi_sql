af9240598720996a4bc4ad93bc54c60baf5dacb8
turko2373 <turko1073@gmail.com>
2023-01-06 02:26:36 -0300

    html funcional

--- www/js/script.js
+++ www/js/script.js
@@ -24,8 +24,7 @@ async function CrearCargar() {
         }
 
         tdOpc = document.createElement("td");
-        tdOpc.innerHTML = 
-        <button class = "btn btn-danger" onclick = "EliminarContactoAgenda(this)"><i class = "far fa-trash-alt"></i></button> ;
+        tdOpc.innerHTML = '<button class = "btn btn-danger" onclick = "EliminarContactoAgenda(this)"><i class = "far fa-trash-alt"></i></button> ';        
         tr.appendChild(tdOpc);
         tabla.appendChild(tr)
     }
@@ -33,22 +32,22 @@ async function CrearCargar() {
 
 
 function EliminarContactoAgenda(btn) {
-    let file = btn.parentNode.parentNode;
+    let fila = btn.parentNode.parentNode;
     let id = fila.firstElementChild.innerHTML;
     let url = "http://127.0.0.1:8000/agenda/";
     alertify.confirm("Se eliminara el contacto de la agenda con el id" + id + "",
-    function() {
-        fetch(url + id, {
-            method: "DELETE"
-        })
-            .then(res => res.json())
-            .catch(err => console.error["Error: ", error])
-            .then(response => console.log("Exito:", response))
-            .then(()=> location.reload())
-        alertify.success("Borrado");
+        function() {
+            fetch(url + id, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .catch(err => console.error("Error: ", error))
+                .then(response => console.log("Exito:", response))
+                .then(()=> location.reload())
+            alertify.success("Borrado");
 
-    }),
-    function () {
-        alertify.error("Cancelado")
-    }
+        },
+        function () {
+            alertify.error("Cancelado")
+        })
 }
